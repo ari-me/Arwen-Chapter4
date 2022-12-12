11145592da982e496f35fe05173725e07de57075
ari-me <119844094+ari-me@users.noreply.github.com>
2022-12-12 10:51:33 +0400

    Delete 1aliencolors.py

--- 1aliencolors.py
+++
@@ -1,6 +0,0 @@
-#Chapter 4 - Exercise 1
-alien_color = "yellow"
-if alien_color == "green": #If the alien is green, the player gets 5 points
-    print("You earned 5 points!")
-else: #otherwise, no output
-    print()
