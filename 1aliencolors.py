ebc7a2e42be7595c937f69e6ae64029e8a1a3dbc
ari-me <119844094+ari-me@users.noreply.github.com>
2022-12-12 10:51:53 +0400

    Add files via upload

---
+++ 1aliencolors.py
@@ -0,0 +1,6 @@
+#Chapter 4 - Exercise 1
+alien_color = "green"
+if alien_color == "green": #If the alien is green, the player gets 5 points
+    print("You earned 5 points!")
+else: #otherwise, no output
+    print()
